6fbd3a4066d36eb989fa7a397eeedb324031e39d
a-whall <awh577868@gmail.com>
2022-03-24 23:08:41 -0400

    cleaned top comment

--- addReviews.js
+++ addReviews.js
@@ -1,12 +1,6 @@
-/** Notes:
- *
- * The search bar is too wide
- *
- * Icons below the image carousel overlap. need fixing
- *
+/**
  * TODO:
  * - find a better way to add icons as a review header
- * - come up with further ideas for review structure
  */
 
 const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
