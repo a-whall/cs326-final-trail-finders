853afd096918f3f84e2f95dd110cf6f1230c19dc
a-whall <awh577868@gmail.com>
2022-04-15 13:52:56 -0400

    crud event listener

---
+++ client/createTrailPage.js
@@ -0,0 +1,16 @@
+
+import * as crud from './crud.js';
+
+const name_input = document.getElementById('name-input');
+const town_input = document.getElementById('town-input');
+const description_input = document.getElementById('description-input');
+const done_button = document.getElementById('done-button');
+
+done_button.addEventListener('click', async(e) => {
+  await crud.createTrail(
+    name_input.value,
+    town_input.value,
+    description_input.value
+  );
+  // TODO: take user to the newly created page
+});
