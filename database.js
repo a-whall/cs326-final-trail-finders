1616925b5d9dc0566f48cd83dbda843fe410ae08
a-whall <awh577868@gmail.com>
2022-04-18 21:52:46 -0400

    comment query code, dummy readTrail response

--- database.js
+++ database.js
@@ -52,10 +52,9 @@ export class TrailFinderDatabase {
       response.status(400).json({ error: args.error });
     } else {
       // create ID or use trail name as ID?
-      const queryText =
-        'INSERT INTO trails (name, town, description) VALUES ($1, $2, $3) RETURNING *';
-      const res = await this.client.query(queryText, [args.name, args.town, args.description]);
-      return res.rows;
+      // const queryText = 'INSERT INTO trails (name, town, description) VALUES ($1, $2, $3) RETURNING *';
+      // const res = await this.client.query(queryText, [args.name, args.town, args.description]);
+      // return res.rows;
     }
   }
   async readTrail(request, response) {
@@ -63,9 +62,11 @@ export class TrailFinderDatabase {
     if ("error" in args) {
       response.status(400).json({ error: args.error });
     } else {
-      const queryText =
-        'SELECT * FROM trails WHERE name = $1';
-      const res = await this.client.query(queryText, [args.name]);
+      // const queryText = 'SELECT * FROM trails WHERE name = $1';
+      // const res = await this.client.query(queryText, [args.name]);
+
+      // dummy response
+      response.status(200).json({ name: args.name, town: "Amherst", description: "This is a placeholder description of a trail until we get the database working.", imageURLs: ["https://photos.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvMjc1NTQ1MTIvMmEyODczMmU3OGMzMmQ1MjA4ODVjMWJlZDEyMGNmODYuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJqcGVnIiwicmVzaXplIjp7IndpZHRoIjo1MDAsImhlaWdodCI6NTAwLCJmaXQiOiJpbnNpZGUifSwicm90YXRlIjpudWxsLCJqcGVnIjp7InRyZWxsaXNRdWFudGlzYXRpb24iOnRydWUsIm92ZXJzaG9vdERlcmluZ2luZyI6dHJ1ZSwib3B0aW1pc2VTY2FucyI6dHJ1ZSwicXVhbnRpc2F0aW9uVGFibGUiOjN9fX0=","https://photos.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvNDE4NzIxMjUvYzgzYWI2ZjVlMWQxNmI5OWQ5MDcyMzk5MjQyZGQwY2IuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJqcGVnIiwicmVzaXplIjp7IndpZHRoIjoyMDQ4LCJoZWlnaHQiOjIwNDgsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsImpwZWciOnsidHJlbGxpc1F1YW50aXNhdGlvbiI6dHJ1ZSwib3ZlcnNob290RGVyaW5naW5nIjp0cnVlLCJvcHRpbWlzZVNjYW5zIjp0cnVlLCJxdWFudGlzYXRpb25UYWJsZSI6M319fQ==","https://photos.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvMjEwMjY5NzMvNDJkZWE3NjM1NWE2OThlMWJlODYyZDUzYmUzNmQ5ZWEuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJqcGVnIiwicmVzaXplIjp7IndpZHRoIjo1MDAsImhlaWdodCI6NTAwLCJmaXQiOiJpbnNpZGUifSwicm90YXRlIjpudWxsLCJqcGVnIjp7InRyZWxsaXNRdWFudGlzYXRpb24iOnRydWUsIm92ZXJzaG9vdERlcmluZ2luZyI6dHJ1ZSwib3B0aW1pc2VTY2FucyI6dHJ1ZSwicXVhbnRpc2F0aW9uVGFibGUiOjN9fX0="]})
     }
   }
   async updateTrail(request, response) {
@@ -73,11 +74,8 @@ export class TrailFinderDatabase {
     if ("error" in args) {
       response.status(400).json({ error: args.error });
     } else {
-      const queryText =
-        `UPDATE trails
-        SET town = $2, description = $3
-        WHERE name = $1`;
-      const res = await this.client.query(queryText, [args.name, args.town, args.description]);
+      // const queryText = `UPDATE trails SET town = $2, description = $3 WHERE name = $1`;
+      // const res = await this.client.query(queryText, [args.name, args.town, args.description]);
     }
   }
   async deleteTrail(request, response) {
@@ -85,8 +83,8 @@ export class TrailFinderDatabase {
     if ("error" in args) {
       response.status(400).json({ error: args.error });
     } else {
-      const queryText = 'DELETE FROM trails WHERE name = $1';
-      const res = await this.client.query(queryText, [args.name]);
+      // const queryText = 'DELETE FROM trails WHERE name = $1';
+      // const res = await this.client.query(queryText, [args.name]);
     }
   }
 }
