f4e042046ac501bc236697373ecc1d46e6c34410
Sonny Mei <sonnywcmei@gmail.com>
2022-04-19 19:34:11 -0400

    implemented eventPage CRUD and API calls

--- database.js
+++ database.js
@@ -52,10 +52,9 @@ export class TrailFinderDatabase {
       response.status(400).json({ error: args.error });
     } else {
       // create ID or use trail name as ID?
-      const queryText =
-        'INSERT INTO trails (name, town, description) VALUES ($1, $2, $3) RETURNING *';
-      const res = await this.client.query(queryText, [args.name, args.town, args.description]);
-      return res.rows;
+      // const queryText = 'INSERT INTO trails (name, town, description) VALUES ($1, $2, $3) RETURNING *';
+      // const res = await this.client.query(queryText, [args.name, args.town, args.description]);
+      // return res.rows;
     }
   }
   async readTrail(request, response) {
@@ -63,9 +62,11 @@ export class TrailFinderDatabase {
     if ("error" in args) {
       response.status(400).json({ error: args.error });
     } else {
-      const queryText =
-        'SELECT * FROM trails WHERE name = $1';
-      const res = await this.client.query(queryText, [args.name]);
+      // const queryText = 'SELECT * FROM trails WHERE name = $1';
+      // const res = await this.client.query(queryText, [args.name]);
+
+      // dummy response
+      response.status(200).json({ name: args.name, town: "Amherst", description: "This is a placeholder description of a trail until we get the database working.", imageURLs: ["https://photos.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvMjc1NTQ1MTIvMmEyODczMmU3OGMzMmQ1MjA4ODVjMWJlZDEyMGNmODYuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJqcGVnIiwicmVzaXplIjp7IndpZHRoIjo1MDAsImhlaWdodCI6NTAwLCJmaXQiOiJpbnNpZGUifSwicm90YXRlIjpudWxsLCJqcGVnIjp7InRyZWxsaXNRdWFudGlzYXRpb24iOnRydWUsIm92ZXJzaG9vdERlcmluZ2luZyI6dHJ1ZSwib3B0aW1pc2VTY2FucyI6dHJ1ZSwicXVhbnRpc2F0aW9uVGFibGUiOjN9fX0=","https://photos.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvNDE4NzIxMjUvYzgzYWI2ZjVlMWQxNmI5OWQ5MDcyMzk5MjQyZGQwY2IuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJqcGVnIiwicmVzaXplIjp7IndpZHRoIjoyMDQ4LCJoZWlnaHQiOjIwNDgsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsImpwZWciOnsidHJlbGxpc1F1YW50aXNhdGlvbiI6dHJ1ZSwib3ZlcnNob290RGVyaW5naW5nIjp0cnVlLCJvcHRpbWlzZVNjYW5zIjp0cnVlLCJxdWFudGlzYXRpb25UYWJsZSI6M319fQ==","https://photos.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvMjEwMjY5NzMvNDJkZWE3NjM1NWE2OThlMWJlODYyZDUzYmUzNmQ5ZWEuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJqcGVnIiwicmVzaXplIjp7IndpZHRoIjo1MDAsImhlaWdodCI6NTAwLCJmaXQiOiJpbnNpZGUifSwicm90YXRlIjpudWxsLCJqcGVnIjp7InRyZWxsaXNRdWFudGlzYXRpb24iOnRydWUsIm92ZXJzaG9vdERlcmluZ2luZyI6dHJ1ZSwib3B0aW1pc2VTY2FucyI6dHJ1ZSwicXVhbnRpc2F0aW9uVGFibGUiOjN9fX0="]})
     }
   }
   async updateTrail(request, response) {
@@ -73,11 +74,8 @@ export class TrailFinderDatabase {
     if ("error" in args) {
       response.status(400).json({ error: args.error });
     } else {
-      const queryText =
-        `UPDATE trails
-        SET town = $2, description = $3
-        WHERE name = $1`;
-      const res = await this.client.query(queryText, [args.name, args.town, args.description]);
+      // const queryText = `UPDATE trails SET town = $2, description = $3 WHERE name = $1`;
+      // const res = await this.client.query(queryText, [args.name, args.town, args.description]);
     }
   }
   async deleteTrail(request, response) {
@@ -85,55 +83,9 @@ export class TrailFinderDatabase {
     if ("error" in args) {
       response.status(400).json({ error: args.error });
     } else {
-      const queryText = 'DELETE FROM trails WHERE name = $1';
-      const res = await this.client.query(queryText, [args.name]);
+      // const queryText = 'DELETE FROM trails WHERE name = $1';
+      // const res = await this.client.query(queryText, [args.name]);
     }
-  }
-}
-
-// Deal with attendance database later
-export class EventPageDatabase {
-  constructor(db_url) {
-    this.dburl = db_url;
-  }
-  async connect() {
-    // Create a new Pool. The Pool manages a set of connections to the database.
-    // It will keep track of unused connections, and reuse them when new queries
-    // are needed. The constructor requires a database URL to make the
-    // connection. You can find the URL of your database by looking in Heroku
-    // or you can run the following command in your terminal:
-    //
-    //  heroku pg:credentials:url -a APP_NAME
-    //
-    // Replace APP_NAME with the name of your app in Heroku.
-    this.pool = new Pool({
-      connectionString: this.dburl,
-      ssl: { rejectUnauthorized: false }, // Required for Heroku connections
-    });
-
-    // Create the pool.
-    this.client = await this.pool.connect();
-
-    // Init the database.
-    await this.init();
-  }
-  async init() {
-    const queryText = `
-      CREATE TABLE IF NOT EXISTS events (
-        eid int primary key,
-        name varchar(64),
-        time varchar(64),
-        meetup varchar(64),
-        uid int,
-        description varchar(1920)
-      );
-      
-      INSERT INTO
-        events(eid, name, time, meetup, uid, description)
-      VALUES
-        (1, 'Norwottuck Rail Trail', '04/06/2022, 4pm to 7pm', 'Amherst Town', 1, 'Let's bike!'),
-        (2, 'The Notch', '04/07/2022, 4pm to 7pm', 'Northhampton', 2, 'Walk trail')`;
-    await this.client.query(queryText);
   }
   async createEvent(request, response) {
     console.log(JSON.stringify(request.body))
@@ -141,10 +93,11 @@ export class EventPageDatabase {
     if ("error" in args) {
       response.status(400).json({ error: args.error });
     } else {
-      const queryText =
-        'INSERT INTO events (eid, name, time, meetup, uid, description) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
-      const res = await this.client.query(queryText, [args.eid, args.name, args.time, args.meetup, args.uid, args.description]);
-      return res.rows;
+      // const queryText =
+        // 'INSERT INTO events (eid, name, time, meetup, uid, description) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
+      // const res = await this.client.query(queryText, [args.eid, args.name, args.time, args.meetup, args.uid, args.description]);
+      // return res.rows;
+      response.status(200).json({ eid: args.eid, name: args.name, time: args.time, meetup: args.meetup, uid: args.uid, description: args.description });
     }
   }
   async readEvent(request, response) {
@@ -152,9 +105,10 @@ export class EventPageDatabase {
     if ("error" in args) {
       response.status(400).json({ error: args.error });
     } else {
-      const queryText =
-        'SELECT * FROM events WHERE eid = $1';
-      const res = await this.client.query(queryText, [args.eid]);
+      // const queryText =
+        // 'SELECT * FROM events WHERE eid = $1';
+      // const res = await this.client.query(queryText, [args.eid]);
+      response.status(200).json({ eid: args.eid});
     }
   }
   async updateEvent(request, response) {  // uid might not be needed up updateevent, as event should correspond to same host/uid
@@ -162,11 +116,12 @@ export class EventPageDatabase {
     if ("error" in args) {
       response.status(400).json({ error: args.error });
     } else {
-      const queryText =
-        `UPDATE events
-        SET name = $2, time = $3, meetup = $4, uid = $5, description = $6
-        WHERE eid = $1`;
-      const res = await this.client.query(queryText, [args.eid, args.name, args.time, args.meetup, args.uid, args.description]);
+      // const queryText =
+        // `UPDATE events
+        // SET name = $2, time = $3, meetup = $4, uid = $5, description = $6
+        // WHERE eid = $1`;
+      // const res = await this.client.query(queryText, [args.eid, args.name, args.time, args.meetup, args.uid, args.description]);
+      response.status(200).json({ eid: args.eid, name: args.name, time: args.time, meetup: args.meetup, uid: args.uid, description: args.description });
     }
   }
   async deleteEvent(request, response) {
@@ -174,8 +129,9 @@ export class EventPageDatabase {
     if ("error" in args) {
       response.status(400).json({ error: args.error });
     } else {
-      const queryText = 'DELETE FROM events WHERE eid = $1';
-      const res = await this.client.query(queryText, [args.eid]);
+      // const queryText = 'DELETE FROM events WHERE eid = $1';
+      // const res = await this.client.query(queryText, [args.eid]);
+      response.status(200).json({ eid: args.eid });
     }
   }
 }
@@ -184,7 +140,7 @@ let dummyDB = {
  users: { 1:{ name:'', } },
  reviews: { 1:{ user:1, body:'', likes:[1,2,3] }, },
  trails: { 1:{ name:'', town:'', description:'', reviews:[1,2,3] } },
- events: { 1:{ name:'', time:'', meetup:'', user:1, description:'' } }
+ events: { 1:{ }}
 };
 
 // crud helper
--- server.js
+++ server.js
@@ -1,51 +1,29 @@
 import express from 'express';
 import logger from 'morgan';
-import { TrailFinderDatabase, EventPageDatabase } from "./database.js";
+import { TrailFinderDatabase } from "./database.js";
 
 class TrailFinderServer {
   constructor(dburl) {
     this.dburl = dburl;
     this.app = express();
     this.app.use(logger('dev'));
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
     this.app.use('/', express.static('client'));
     return this;
   }
 
   async initRoutes() {
     this.app.post('/trail', this.db.createTrail);
+    this.app.post('/event', this.db.createEvent);
+    this.app.get('/event', this.db.readEvent);
+    this.app.put('/event', this.db.updateEvent);
+    this.app.delete('/event', this.db.deleteEvent);
   }
 
   async initDb() {
     this.db = new TrailFinderDatabase(this.dburl);
-    await this.db.connect();
-  }
-
-  async start() {
-    await this.initDb();
-    await this.initRoutes();
-    const port = process.env.PORT || 3000;
-    this.app.listen(port, () => {
-      console.log(`Server started on port ${port}!`);
-    });
-  }
-}
-
-class EventPageServer {
-  constructor(dburl) {
-    this.dburl = dburl;
-    this.app = express();
-    this.app.use(logger('dev'));
-    this.app.use('/', express.static('client'));
-    return this;
-  }
-
-  async initRoutes() {
-    this.app.post('/event', this.db.createEvent);
-  }
-
-  async initDb() {
-    this.db = new EventPageDatabase(this.dburl);
-    await this.db.connect();
+    // await this.db.connect();
   }
 
   async start() {
@@ -59,5 +37,3 @@ class EventPageServer {
 }
 
 new TrailFinderServer(process.env.DATABASE_URL).start();
-
-new EventPageServer(process.env.DATABASE_URL).start();
