9796b2ba01c66549569a358f7981bbc9f6c7a55e
a-whall <awh577868@gmail.com>
2022-04-20 10:13:21 -0400

    createTrail success response, readTrails

--- database.js
+++ database.js
@@ -46,7 +46,6 @@ export class TrailFinderDatabase {
     await this.client.query(queryText);
   }
   async createTrail(request, response) {
-    console.log(JSON.stringify(request.body))
     const args = parse(request.body, "name", "town", "description");
     if ("error" in args) {
       response.status(400).json({ error: args.error });
@@ -55,6 +54,7 @@ export class TrailFinderDatabase {
       // const queryText = 'INSERT INTO trails (name, town, description) VALUES ($1, $2, $3) RETURNING *';
       // const res = await this.client.query(queryText, [args.name, args.town, args.description]);
       // return res.rows;
+      response.status(200).json({ status:"success" });
     }
   }
   async readTrail(request, response) {
@@ -64,27 +64,17 @@ export class TrailFinderDatabase {
     } else {
       // const queryText = 'SELECT * FROM trails WHERE name = $1';
       // const res = await this.client.query(queryText, [args.name]);
-
-      // dummy response
       response.status(200).json({ name: args.name, town: "Amherst", description: "This is a placeholder description of a trail until we get the database working.", imageURLs: ["https://photos.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvMjc1NTQ1MTIvMmEyODczMmU3OGMzMmQ1MjA4ODVjMWJlZDEyMGNmODYuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJqcGVnIiwicmVzaXplIjp7IndpZHRoIjo1MDAsImhlaWdodCI6NTAwLCJmaXQiOiJpbnNpZGUifSwicm90YXRlIjpudWxsLCJqcGVnIjp7InRyZWxsaXNRdWFudGlzYXRpb24iOnRydWUsIm92ZXJzaG9vdERlcmluZ2luZyI6dHJ1ZSwib3B0aW1pc2VTY2FucyI6dHJ1ZSwicXVhbnRpc2F0aW9uVGFibGUiOjN9fX0=","https://photos.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvNDE4NzIxMjUvYzgzYWI2ZjVlMWQxNmI5OWQ5MDcyMzk5MjQyZGQwY2IuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJqcGVnIiwicmVzaXplIjp7IndpZHRoIjoyMDQ4LCJoZWlnaHQiOjIwNDgsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsImpwZWciOnsidHJlbGxpc1F1YW50aXNhdGlvbiI6dHJ1ZSwib3ZlcnNob290RGVyaW5naW5nIjp0cnVlLCJvcHRpbWlzZVNjYW5zIjp0cnVlLCJxdWFudGlzYXRpb25UYWJsZSI6M319fQ==","https://photos.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvMjEwMjY5NzMvNDJkZWE3NjM1NWE2OThlMWJlODYyZDUzYmUzNmQ5ZWEuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJqcGVnIiwicmVzaXplIjp7IndpZHRoIjo1MDAsImhlaWdodCI6NTAwLCJmaXQiOiJpbnNpZGUifSwicm90YXRlIjpudWxsLCJqcGVnIjp7InRyZWxsaXNRdWFudGlzYXRpb24iOnRydWUsIm92ZXJzaG9vdERlcmluZ2luZyI6dHJ1ZSwib3B0aW1pc2VTY2FucyI6dHJ1ZSwicXVhbnRpc2F0aW9uVGFibGUiOjN9fX0="]})
     }
   }
-  async updateTrail(request, response) {
-    const args = parse(request.query, "trail_id", "name", "town", "description");
+  async readTrails(request, response) {
+    const args = parse(request.query, "town", "offset");
     if ("error" in args) {
       response.status(400).json({ error: args.error });
     } else {
-      // const queryText = `UPDATE trails SET town = $2, description = $3 WHERE name = $1`;
-      // const res = await this.client.query(queryText, [args.name, args.town, args.description]);
-    }
-  }
-  async deleteTrail(request, response) {
-    const args = parse(request.query, "name");
-    if ("error" in args) {
-      response.status(400).json({ error: args.error });
-    } else {
-      // const queryText = 'DELETE FROM trails WHERE name = $1';
-      // const res = await this.client.query(queryText, [args.name]);
+      // const queryText = `SELECT * FROM trails LIMIT 10 OFFSET $1`;
+      // const res = await this.client.query(queryText, [args.offset * 10]);
+      response.status(200).json(["The Notch", "Amethyst Brook Conservation Area", "Rattlesnake Gutter", "Mount Toby State Forest", "Norwottuch Rail Trail", "Mill River Conservation Area", "Skinner State Park"]);
     }
   }
 }
