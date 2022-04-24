081d6cd6c778e0703f501a3b73db54df82b3948a
a-whall <awh577868@gmail.com>
2022-04-24 17:15:51 -0400

    change event listener and get file input element

--- client/createTrailPage.js
+++ client/createTrailPage.js
@@ -1,16 +1,24 @@
-
 import * as crud from './crud.js';
 
+const file_input = document.getElementById('image-file');
 const name_input = document.getElementById('name-input');
 const town_input = document.getElementById('town-input');
 const description_input = document.getElementById('description-input');
 const done_button = document.getElementById('done-button');
 
 done_button.addEventListener('click', async(e) => {
-  await crud.createTrail(
-    name_input.value,
-    town_input.value,
-    description_input.value
-  );
-  window.location.href = "./trailPage.html?trail="+name_input.value;
+  const form_data = new FormData();
+  for(const file of file_input.files) {
+    form_data.append(file.name, file);
+  }
+  
+  await crud.uploadTrailImage('trailname',form_data);
+  //window.location.href = "./trailPage.html?trail="+name_input.value;
 });
+
+// function base64_encode(file) {
+//   const img_str = fs.readFileSync(file, { encoding: 'base64' });
+//   console.log(img_str.length)
+// }
+
+// base64_encode('C:\cs326\cs326-final-trail-finders\client\assets\Valley_Trail_Finder_Logo.JPG')
