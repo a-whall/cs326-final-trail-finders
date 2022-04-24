cf7b07b257020fb528123b8c2cc934e7db80b8ba
a-whall <awh577868@gmail.com>
2022-04-24 17:18:15 -0400

    express-fileupload middleware

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import upload from 'express-fileupload';
 import logger from 'morgan';
+
 import { TrailFinderDatabase } from "./database.js";
 
 class TrailFinderServer {
@@ -7,6 +9,7 @@ class TrailFinderServer {
     this.dburl = dburl;
     this.app = express();
     this.app.use(logger('dev'));
+    this.app.use(upload());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
     this.app.use('/', express.static('client'));
@@ -16,6 +19,7 @@ class TrailFinderServer {
   async initRoutes() {
     this.app.get('/', function(req,res) { res.redirect('/homepage.html'); });
     this.app.post('/trail', this.db.createTrail.bind(this.db));
+    this.app.post('/trail/image', this.db.createTrailImage.bind(this.db));
     this.app.get('/trail', this.db.readTrail.bind(this.db));
     this.app.get('/trail/browse', this.db.readTrails.bind(this.db));
     this.app.get('/trail/count', this.db.readTrailCount.bind(this.db));
