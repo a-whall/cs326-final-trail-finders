1e6c39ab0f05c9b24b26aca6081ed8c5bce0d718
Sonny Mei <sonnywcmei@gmail.com>
2022-04-26 22:58:20 -0400

    trying to make createEvent work from client to server to database attemp#7

--- database.js
+++ database.js
@@ -157,12 +157,13 @@ export class TrailFinderDatabase {
   async createEvent(request, response) {
     const args = parse(request.body, "title", "time", "meetup", "username", "description", "trail");
     console.log(args);
+    const eid = 2;
     if ("error" in args) {
       response.status(400).json({ error: args.error });
     } else {
       const queryText =
-        'INSERT INTO events (title, time, meetup, username, description, trail) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
-      const res = await this.client.query(queryText, [args.title, args.time, args.meetup, args.username, args.description, args.trail]);
+        'INSERT INTO events (eid, title, time, meetup, username, description, trail) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *';
+      const res = await this.client.query(queryText, [eid, args.title, args.time, args.meetup, args.username, args.description, args.trail]);
       return res.rows;
       // response.status(200).json({ eid: 0, title: args.title, time: args.time, meetup: args.meetup, username: args.username, description: args.description });
     }
