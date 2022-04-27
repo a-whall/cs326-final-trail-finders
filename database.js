9adaa76578496083ee9cb6cc70eec13e3344cba3
Sonny Mei <sonnywcmei@gmail.com>
2022-04-26 21:37:29 -0400

    minor edits to database.js regarding event table

--- database.js
+++ database.js
@@ -69,10 +69,10 @@ export class TrailFinderDatabase {
         );
         
         INSERT INTO
-          events(eid, title, time, meetup, username, description, trail)
+          events(title, time, meetup, username, description, trail)
         VALUES
-          (1, 'Norwottuck Rail Trail Event!', '04/06/2022, 4pm to 7pm', 'Amherst Town', 'Amanda', 'Lets bike!', 'Norwottuck Rail Trail'),
-          (2, 'The Notch Event!', '04/07/2022, 4pm to 7pm', 'Northhampton', 'Joe', 'Walk trail', 'The Notch')`;
+          ('Norwottuck Rail Trail Event!', '04/06/2022, 4pm to 7pm', 'Amherst Town', 'Amanda', 'Lets bike!', 'Norwottuck Rail Trail'),
+          ('The Notch Event!', '04/07/2022, 4pm to 7pm', 'Northhampton', 'Joe', 'Walk trail', 'The Notch')`;
     await this.client.query(queryText);
   }
   async createTrail(request, response) {
