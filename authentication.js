13c081bdc0884717a13f91f9b39358117e00924d
a-whall <awh577868@gmail.com>
2022-04-29 20:51:13 -0400

    clean up

--- authentication.js
+++ authentication.js
@@ -1,61 +1,31 @@
-/************************* authentication stuff *************************************/
-
 import passport from 'passport';
 import passportLocal from 'passport-local';
 import 'dotenv/config';
 import * as crud from './client/crud.js'
 
-
-
 const { Strategy } = passportLocal;
 
-// Passport Configuration
-// Create a new LocalStrategy object to handle authentication using username and
-// password credentials from the client. The LocalStrategy object is used to
-// authenticate a user using a username and password.
-const strategy = new Strategy(async (username, password, done) => {
+const localStrategy = new Strategy(async (username, password, done) => {
   let userExists = await crud.checkUser(username);
   if (!userExists) {
-    // no such user
     return done(null, false, { message: 'Invalid username' });
   }
   let validUserProfile = await crud.readUser(username, password);
   if (!validUserProfile) {
-    // invalid password
-    // should disable logins after N messages
-    // delay return to rate-limit brute-force attacks
     await new Promise((r) => setTimeout(r, 2000)); // two second delay
     return done(null, false, { message: 'Wrong password' });
   }
-  // success!
-  // should create a user object here, associated with a unique identifier
   return done(null, username);
 });
 
-// Configure passport to use the LocalStrategy object.
-// The LocalStrategy object is used to authenticate a user using a username and
-// password. There are other strategies available, but this is the simplest.
-passport.use(strategy);
-
-// Convert user object to a unique identifier.
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-
-// Convert a unique identifier to a user object.
-passport.deserializeUser((uid, done) => {
-  done(null, uid);
-});
+passport.use(localStrategy);
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((uid, done) => done(null, uid));
 
 export default {
   configure: (app) => {
     app.use(passport.initialize());
     app.use(passport.session());
   },
-
-  authenticate: (domain, where) => {
-    return passport.authenticate(domain, where);
-  },
+  authenticate: (domain, where) => passport.authenticate(domain, where)
 };
-
-
