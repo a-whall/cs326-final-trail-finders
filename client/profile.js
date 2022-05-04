3d22710c9bd7ea2f13a93f3274934a0d1e3aa47a
a-whall <awh577868@gmail.com>
2022-05-04 11:17:47 -0400

    clean up profile page, unify css

--- client/profile.js
+++ client/profile.js
@@ -7,39 +7,39 @@ const oldPassword = document.getElementById("oldPassword-input");
 const password = document.getElementById("password-input");
 const passwordVer = document.getElementById("passwordVer-input");
 const submit = document.getElementById("update-password-submit");
-const delete_profile = document.getElementById("delete");
+const delete_profile = document.getElementById("delete-btn");
 
 
 home.addEventListener('click', async() => {
-    window.location.href="homepage.html";
+  window.location.href="homepage.html";
 });
 
 trails.addEventListener('click', async() => {
-    window.location.href="browseTrails.html";
+  window.location.href="browseTrails.html";
 });
 
 events.addEventListener('click', async() => {
-    window.location.href="eventPage.html";
+  window.location.href="eventPage.html";
 });
 
 
 passwordVer.addEventListener("keyup", function (event) {
-    if (password.value === passwordVer.value){
-        passwordVer.style.backgroundColor = "green";
-        passwordVer.style.color = "black";
-    }
-    else{
-        passwordVer.style.backgroundColor = "red";
-        passwordVer.style.color = "black";
-    }
+  if (password.value === passwordVer.value){
+    passwordVer.style.backgroundColor = "green";
+    passwordVer.style.color = "black";
+  }
+  else{
+    passwordVer.style.backgroundColor = "red";
+    passwordVer.style.color = "black";
+  }
 });
 
 submit.addEventListener("click", async() =>{
-    const data = await crud.updateUser(oldPassword.value, password.value);
-    (data.status === 'success')? alert('Your password has been changed.'): alert(`error: ${data.status}`);
+  const data = await crud.updateUser(oldPassword.value, password.value);
+  (data.status === 'success')? alert('Your password has been changed.'): alert(`error: ${data.status}`);
 });
 
 delete_profile.addEventListener("click", async() => {
-    const data = await crud.deleteUser();
-    (data.status === 'success')? alert('Your profile has been deleted.'): alert(`error: ${data.status}`);
+  const data = await crud.deleteUser();
+  (data.status === 'success')? alert('Your profile has been deleted.'): alert(`error: ${data.status}`);
 })
