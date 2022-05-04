ad776880b575610ab4a640b191268ffb5df94d85
a-whall <awh577868@gmail.com>
2022-05-04 00:34:17 -0400

    connected to backend via crud, added some todos

--- client/homepage.js
+++ client/homepage.js
@@ -7,33 +7,40 @@ const account_dropdown_loggedIn = document.getElementById('account-logged-in');
 const account_dropdown_loggedOut = document.getElementById('account-logged-out');
 const welcome_container = document.getElementById('welcome-container');
 
-const loggedIn = false;
+positionWelcomeMessage();
 
-if (loggedIn) {
+// choose which dropdown menu to show initially
+const loggedIn = await crud.checkLoggedIn();
+if (loggedIn.value) {
   account_dropdown_loggedOut.classList.add('d-none');
 } else {
   account_dropdown_loggedIn.classList.add('d-none');
 }
 
 login_btn.addEventListener('click', async(e) => {
-  // const data = await crud.createLogin(new FormData(login_form));
-  if (true) {
-    account_dropdown_loggedOut.classList.add('d-none');
+  const login = await crud.createLogin(new FormData(login_form));
+  if (login.status === 'success') {
+    // TODO: change the loggedIn dropdown to display username instead of "account"
     account_dropdown_loggedIn.classList.remove('d-none');
+    account_dropdown_loggedOut.classList.add('d-none');
+  } else {
+    // TODO: display why login failed and keep the account dropdown open
   }
 });
 
 logout_btn.addEventListener('click', async(e) => {
-  // await crud.logout();
+  await crud.logout();
   account_dropdown_loggedIn.classList.add('d-none');
   account_dropdown_loggedOut.classList.remove('d-none');
 });
 
-window.addEventListener('resize', function () {
+window.addEventListener('resize', positionWelcomeMessage, false);
+
+function positionWelcomeMessage() {
   const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-  if (viewportWidth > 1130) {
+  if (viewportWidth > 1395) {
     welcome_container.classList.add('fixed-bottom');
   } else {
     welcome_container.classList.remove('fixed-bottom');
   }
-}, false);
+}
