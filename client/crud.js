6a69f23e2537b961480bcdee31c24ee3d5be203a
a-whall <awh577868@gmail.com>
2022-05-05 12:48:45 -0400

    createUser now sends form data

--- client/crud.js
+++ client/crud.js
@@ -157,14 +157,9 @@ export async function deleteEvent(eid) {
 
 //======================== User =============================================================================================
 
-export async function createUser(username, password) {
-  const response = await fetch('/register', {
-    method: 'POST',
-    headers: { 'Content-Type':'application/json' },
-    body: JSON.stringify({ username:username, password:password })
-  });
-  const data = await response.json();
-  return data;
+export async function createUser(formData) {
+  const response = await fetch('/register', { method: 'POST', body: formData });
+  return await response.json();
 }
 
 export async function deleteUser(username, password) {
