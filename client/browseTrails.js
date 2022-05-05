ac54b9aeef436bf5640fcd198bd416ee358e3d3c
a-whall <awh577868@gmail.com>
2022-05-05 12:50:50 -0400

    dom element constructor, conformed variable names

--- client/browseTrails.js
+++ client/browseTrails.js
@@ -5,27 +5,23 @@ const trail_list = document.getElementById('trail-list');
 const index_list = document.getElementById('page-index');
 
 // Parsed query data
-const page_index = parseInt(new URLSearchParams(window.location.search).get('page')) || 1;
-const town_name = new URLSearchParams(window.location.search).get('trail') || "";
+const pageIndex = parseInt(new URLSearchParams(window.location.search).get('page')) || 1;
+const townName = new URLSearchParams(window.location.search).get('trail') || "";
 
 // Server data
-const trail_data = await crud.readTrailsByTownName(town_name, page_index);
-const trail_table_size = await crud.readTrailsCount();
+const trailData = await crud.readTrailsByTownName(townName, pageIndex);
+const trailTableSize = await crud.readTrailsCount();
 
 // Create a list of links that will bring the user to view individual trails
-for (const trail of trail_data) {
-  const link = document.createElement('a');
-  link.classList.add('text-center');
-  link.href=`./trailPage.html?trail=${trail.name}`;
-  link.innerHTML = `<h3>${trail.name}</h3>`;
-  trail_list.append(link);
+for (const trail of trailData) {
+  trail_list.append(searchResult(trail));
 }
 
 // Create a list of page links
-let num_pages = trail_table_size / 10;
-for (let i = 1; i <= num_pages + 1; i++) {
+let numPages = trailTableSize / 10;
+for (let i = 1; i <= numPages + 1; i++) {
   const td = document.createElement('td');
-  if (i === page_index)
+  if (i === pageIndex)
     td.innerHTML = i;
   else {
     const link = document.createElement('a');
@@ -35,4 +31,26 @@ for (let i = 1; i <= num_pages + 1; i++) {
     td.append(link);
   }
   index_list.append(td);
+}
+
+/**
+ * Construct a dom element for displaying a single search result
+ * @param {object} trail an object with a name, town, and description
+ * @returns a row div element to append to a bootstrap grid
+ */
+function searchResult(trail) {
+  const div = document.createElement('div');
+  div.classList.add('row','my-3','px-5','justify-content-center');
+  const col = document.createElement('div');
+  col.classList.add('col-8','pl-4');
+  const p = document.createElement('p');
+  p.classList.add('description-preview');
+  p.innerText = trail.description;
+  const link = document.createElement('a');
+  link.classList.add('text-nowrap');
+  link.href = `./trailPage.html?trail=${trail.name}`;
+  link.innerHTML = `<h3>${trail.name}</h3>`;
+  col.append(link, p);
+  div.append(col);
+  return div;
 }
