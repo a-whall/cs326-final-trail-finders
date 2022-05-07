a9dabb94b42c2dcd184c3a081049fbf3d9b9013b
a-whall <awh577868@gmail.com>
2022-05-07 15:07:50 -0400

    error checking for empty fields and authentication

--- client/createTrailPage.js
+++ client/createTrailPage.js
@@ -7,13 +7,19 @@ const description_input = document.getElementById('description-input');
 const done_button = document.getElementById('done-button');
 
 done_button.addEventListener('click', async(e) => {
-  const trail_created = await crud.createTrail(name_input.value, town_input.value, description_input.value);
-  if (trail_created) {
+  if (!name_input.value || !town_input.value || !description_input.value) {
+    alert('Please provide a trail name, town name, and description');
+    return;
+  }
+  const trailCreated = await crud.createTrail(name_input.value, town_input.value, description_input.value);
+  if (trailCreated.status === 'success') {
     const form_data = new FormData();
     for (const file of file_input.files) {
       form_data.append(file.name, file);
     }
     await crud.uploadTrailImage(name_input.value, form_data);
+    window.location.href = `./trailPage.html?trail=${name_input.value}`;
+  } else {
+    alert(trailCreated.status);
   }
-  window.location.href = `./trailPage.html?trail=${name_input.value}`;
 });
